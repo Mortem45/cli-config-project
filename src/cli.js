a74c43030938eed6c5d9647f306ad2961821d492
mortem45 <ste45_007@hotmail.com>
2021-11-06 17:40:25 -0600

    create project logic

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 'use strict'
 
 import arg from 'arg'
+import inquirer from 'inquirer'
+import { createProject } from './main'
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
@@ -23,7 +25,47 @@ function parseArgumentsIntoOptions(rawArgs) {
   }
 }
 
-export function cli(args) {
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'JavaScript'
+  if (options.skipPrompts){
+    return {
+      ...options,
+      template: options.template || defaultTemplate
+    }
+  }
+
+  const questions = []
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Please choose which project template to use',
+      choices: ['JavaScript', 'TypeScript'],
+      default: defaultTemplate
+    })
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository?',
+      default: false
+    })
+  }
+
+  const answers = await inquirer.prompt(questions)
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git
+  }
+
+}
+
+export async function cli(args) {
   let options = parseArgumentsIntoOptions(args)
-  console.log(options)
+  options = await promptForMissingOptions(options)
+  console.table(options);
+  await createProject(options)
 }
